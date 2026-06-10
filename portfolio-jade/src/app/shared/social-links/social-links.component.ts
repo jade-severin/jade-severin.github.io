97e3b918c005e64666af5c23296d5bb6d84d9b87
Joan <j.massart.severin@proton.me>
2026-06-10 16:26:58 +0200

    email to clipboard

--- portfolio-jade/src/app/shared/social-links/social-links.component.ts
+++ portfolio-jade/src/app/shared/social-links/social-links.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-social-links',
@@ -7,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './social-links.component.scss',
 })
 export class SocialLinksComponent {
-  protected instagramUrl = 'https://www.instagram.com/piano.cktail/';
-  protected linkedInUrl = 'https://fr.linkedin.com/in/jade-severin-com-culture';
-  protected cvUrl = 'SEVERIN_Jade_CV.pdf';
+  protected readonly instagramUrl = 'https://www.instagram.com/piano.cktail/';
+  protected readonly linkedInUrl = 'https://fr.linkedin.com/in/jade-severin-com-culture';
+  protected readonly cvUrl = 'SEVERIN_Jade_CV.pdf';
+  protected readonly email = 'severinjade@gmail.com';
+
+  protected readonly showToast = signal(false);
+
+  protected readonly hidingToast = signal(false);
+
+  private timeoutId?: number;
+
+  async copyEmail(): Promise<void> {
+    try {
+      await navigator.clipboard.writeText(this.email);
+
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+      }
+
+      this.hidingToast.set(false);
+      this.showToast.set(true);
+
+      this.timeoutId = window.setTimeout(() => {
+        this.hidingToast.set(true);
+
+        window.setTimeout(() => {
+          this.showToast.set(false);
+        }, 250);
+      }, 2200);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
